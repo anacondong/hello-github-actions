88f1048231a74ae292782d00ea474fc95cde7094
anacondong <westkitseo@gmail.com>
2021-08-11 22:28:10 +0700

    update

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME"
+sh -c "echo Hello world my name is $INPUT_MY_NAME "
+sh -c "echo name from github workflows "
+sh -c "echo This Command run with docker to sh script"
